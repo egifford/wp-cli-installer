456747a419fc711151c3c94d7ee7c8c660d1b8f3
Ed Gifford <egifford@blueblazeassociates.com>
2016-11-03 16:39:39 -0400

    Making a dedicated file for install text messages and creating a global text file.

---
+++ utils/install-text.bash
@@ -0,0 +1,10 @@
+source ${UTILS_DIR}text.bash
+
+VERSION_TEXT="WP-CLI installer, version $VERSION"
+
+HELP_TEXT="$VERSION_TEXT
+
+This script will install WP-CLI using Composer. This type of installation isn't as compact as a PHAR
+installation but does make it easy to move between versions.
+
+usage: install-via-composer.sh [--version] [--help]"
--- utils/text.bash
+++ utils/text.bash
@@ -1,9 +1,1 @@
 VERSION="0.25.0"
-VERSION_TEXT="WP-CLI installer, version $VERSION"
-
-HELP_TEXT="$VERSION_TEXT
-
-This script will install WP-CLI using Composer. This type of installation isn't as compact as a PHAR
-installation but does make it easy to move between versions.
-
-usage: install-via-composer.sh [--version] [--help]"
